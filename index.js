97db51efa2946e5d656dc8a5d6dbd44f682e992e
Gabriel Biz <gabrielbizcontato@gmail.com>
2022-07-02 08:17:27 -0300

    Segundo comite

---
+++ index.js
@@ -0,0 +1,100 @@
+// const Numbers = [1, 3, 8, 2, 6, 5, 7]
+// for (let number of Numbers) {
+//   console.log(number)
+// }
+
+// let Pessoa = {
+//   nome: "Gabriel",
+//   idade: 21,
+
+// }
+// console.log(Pessoa.nome)
+
+// const pessoas =
+//   [
+//     {
+//       "nome": "Alana Vera Fogaça",
+//       "idade": 19,
+//       "cpf": "936.178.679-28",
+//       "rg": "31.180.079-8",
+//       "data_nasc": "15/02/2003",
+//       "sexo": "Feminino",
+//       "signo": "Aquário",
+//       "mae": "Valentina Sueli",
+//       "pai": "Martin Renan Fogaça",
+//       "email": "alana.vera.fogaca@demetriushairstudio.com.br",
+//       "senha": "iCpsHCnfqx",
+//       "cep": "65910-739",
+//       "endereco": "Rua B-4",
+//       "numero": 380,
+//       "bairro": "Jardim Tropical",
+//       "cidade": "Imperatriz",
+//       "estado": "MA",
+//       "telefone_fixo": "(98) 3779-2139",
+//       "celular": "(98) 99779-8901",
+//       "altura": "1,60",
+//       "peso": 50,
+//       "tipo_sanguineo": "O-",
+//       "cor": "roxo"
+//     },
+//     {
+//       "nome": "Manoel Gustavo Rafael Pires",
+//       "idade": 75,
+//       "cpf": "707.354.189-00",
+//       "rg": "12.971.955-9",
+//       "data_nasc": "26/03/1947",
+//       "sexo": "Masculino",
+//       "signo": "Áries",
+//       "mae": "Mariah Priscila Ayla",
+//       "pai": "Fábio Miguel Iago Pires",
+//       "email": "manoel.gustavo.pires@lukin4.com.br",
+//       "senha": "kq9jnrQsdl",
+//       "cep": "70295-000",
+//       "endereco": "Quadra SQS 216",
+//       "numero": 341,
+//       "bairro": "Asa Sul",
+//       "cidade": "Brasília",
+//       "estado": "DF",
+//       "telefone_fixo": "(61) 3846-2560",
+//       "celular": "(61) 99978-0179",
+//       "altura": "1,97",
+//       "peso": 104,
+//       "tipo_sanguineo": "A-",
+//       "cor": "preto"
+//     },
+//     {
+//       "nome": "Daiane Isabelly Débora da Costa",
+//       "idade": 67,
+//       "cpf": "266.629.690-36",
+//       "rg": "19.499.350-4",
+//       "data_nasc": "21/05/1955",
+//       "sexo": "Feminino",
+//       "signo": "Gêmeos",
+//       "mae": "Rebeca Tânia Cecília",
+//       "pai": "Bryan Martin da Costa",
+//       "email": "daiane.isabelly.dacosta@hersa.com.br",
+//       "senha": "maUHwNUYzB",
+//       "cep": "69907-844",
+//       "endereco": "Rua 15 de Novembro",
+//       "numero": 207,
+//       "bairro": "Belo Jardim I",
+//       "cidade": "Rio Branco",
+//       "estado": "AC",
+//       "telefone_fixo": "(68) 3699-3199",
+//       "celular": "(68) 98854-5161",
+//       "altura": "1,69",
+//       "peso": 53,
+//       "tipo_sanguineo": "AB+",
+//       "cor": "amarelo"
+//     }
+//   ]
+
+// function pessoasFiltrada(pessoa) {
+//   return pessoa.peso >= 70;
+
+// }
+
+// let idadeFiltrada = pessoas.filter(pessoasFiltrada)
+// console.log(idadeFiltrada)
+
+
